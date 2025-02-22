74b323ec4b6f7ad5f0475360a126a33cdc6bd3ca
anikapai <pai57@purdue.edu>
2025-02-22 18:51:57 -0500

    training model and test sets

--- train_model.py
+++ train_model.py
@@ -27,3 +27,7 @@ def preprocess_text(text):
     return ' '.join(words)
 
 data['cleaned_text'] = data['text'].apply(preprocess_text)
+
+X = data['cleaned_text']
+y = data['label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
