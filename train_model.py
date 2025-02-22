8da730df9ee8bb7adcfd4e5b514b05345023e6c7
abhishekbandaru0 <bandaru0@purdue.edu>
2025-02-22 18:44:56 -0500

    preprocessing text definition and implementation

--- train_model.py
+++ train_model.py
@@ -6,7 +6,24 @@ import pickle
 
 real_news = pd.read_csv('True.csv')
 fake_news = pd.read_csv('Fake.csv')
-
 real_news['label'] = 1
 fake_news['label'] = 0
 data = pd.concat([real_news, fake_news], ignore_index=True)
+
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+def preprocess_text(text):
+    text = re.sub(r'\W', ' ', text)
+    text = re.sub(r'\d+', '', text)
+    text = text.lower()
+    words = word_tokenize(text)
+    words = [word for word in words if word not in stopwords.words('english')]
+    return ' '.join(words)
+
+data['cleaned_text'] = data['text'].apply(preprocess_text)
